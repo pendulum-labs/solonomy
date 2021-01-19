5f71cd15023678721c6cc1df218c3951dbc9038c
Charles Dusek <cgdusek@gmail.com>
2021-01-18 20:29:36 -0600

    Saving progress

--- contracts/BondingNOM.sol
+++ contracts/BondingNOM.sol
@@ -19,24 +19,37 @@ contract BondingNOM {
     uint256 public a = 100000000;
 
     // Conversion from token to 64x64
-    function tokenToFixed64(uint256 token) public view returns(int128) {
+    function DecToF64(uint256 token) public view returns(int128) {
         return int128(token)*10**(int128(prec) - int128(decimals))
     }
 
     // Convert Fixed Point 64 to Token UInt256
-    function fixed64ToToken(int128 fixed64) public view returns(uin256) {
+    function f64ToDec(int128 fixed64) public view returns(uin256) {
         return uint256(fixed64/10**(int128(prec) - int128(decimals)))
     }
 
     // ETH/NOM = (#NOM Sold/a)^2
+    // At 18 decimal precision
     function bondCurvePrice() public view returns(uint256) {
-        return fixed64ToToken(pow(div(tokenToFixed65(numNOMSold)/abdk64.fromUInt(a))),uint256(2));
+        return f64ToDec(
+                pow(
+                    div(
+                        DecToF64(numNOMSold),
+                        abdk64.fromUInt(a)
+                    ),
+                    uint256(2)
+                )
+            );
     }
 
     // #NOM Sold = sqrt(ETH/NOM) * a
     // Input 64.64 fixed point number
     function supplyAtPrice(uint256 price) return(uint256) {
-        return abdk64.sqrt(abdk64.fromUINT(price >> (uint256(prec) - uint256(decimals))))*fromUINT(a)
+        return f64toDec(
+                abdk64.sqrt(
+                    abdk64.fromUINT(price >> (uint256(prec) - uint256(decimals))
+                )
+            )*fromUINT(a));
     }
 
     // Integrate over curve to get amount of ETH
